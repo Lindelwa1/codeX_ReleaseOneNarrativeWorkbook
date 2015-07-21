3300da9ac1e3b568339ea621463c2687c0aaa27b
Lindelwa1 <lindelwa@projectcodex.co>
2015-07-21 08:57:17 +0200

    changes saved

--- node-tutorial/products.js
+++ node-tutorial/products.js
@@ -4,82 +4,68 @@
   var findProducts = function (fileName) {
       var productNames= []; //create a place to store productNames
       var productMap = {};
-      var itemsMap ={};
-      var quantitySold = [];
-    // body...
-
+      
+      // body...
       var fileContent = fs.readFileSync(fileName, "utf8");// reading the csv using asyn method
       var products = fileContent.split("\n");//split with new line the file content that we just read
     
-       products.forEach( function (product) { //how you looping through each of the products
-         // body...
-        var hold = product.split(",");// spliting columns with comas
-        var productName = hold[1]; //telling you  to hold the second column
-        var quantitySold = hold[2]; //telling you  to hold the the third column
-        
-
-       if (productMap[productName] === undefined && productName != undefined) {//this is map method that used to check if u hv seen the product previous
-          productNames.push(productName);//it its a new product it gets pushed to the array
-          productMap[productName] = 0;//initial value of product is zero
-                 };
+      products.forEach( function (product) { //how you looping through each of the products
+           // body...
+          var hold = product.split(",");// spliting columns with comas
+          var productName = hold[1]; //telling you  to hold the second column
+          //var quantitySold = hold[2]; //telling you  to hold the the third column
+          
+         if (productMap[productName] === undefined && productName != undefined) {//this is map method that used to check if u hv seen the product previous
+            productNames.push(productName);//it its a new product it gets pushed to the array
+            productMap[productName] = 0;//initial value of product is zero
+          };
       });
-        console.log(productNames);
-        console.log(quantitySold);
-        return productNames;
+      console.log(productNames);
+      
+      return productNames;
   }
 
   var findProductsSold = function (fileName) {
-    var productNames= [];
-    var productMap = {};
-    var itemsMap ={};
-    var quantitySold = [];
-    // body...
+      var productNames= [];
+      var productsMap = {};
+      var quantitySold = [];
+      // body...
 
-    var fileContent = fs.readFileSync(fileName, "utf8");
-    var products = fileContent.split("\n");
-    
-    products.forEach( function (product) {
-        
-        var hold = product.split(",");
-        var productName = hold[1];
-        var quantity =hold[2];
-        
-        if (productMap[productName] === undefined && productName != undefined) {
-          productNames.push(productName);
-          productMap[productName] = 0;
-            
-        };
+      var fileContent = fs.readFileSync(fileName, "utf8");
+      var products = fileContent.split("\n");
+      
+      products.forEach( function (product) {
+          
+          var productColumns = product.split(",");
+          var productName = productColumns[1];
+          var quantity = Number(productColumns[2]);
 
-        var prod=product.split(",");
-        var soldItems = prod[1];
-        if (itemsMap[soldItems] === undefined && itemsMap != undefined) {
-              quantitySold.push(soldItems);
-              itemsMap[soldItems] = 0;
-              var totalItems = parseInt(soldItems,9);
+          if (productsMap[productName] === undefined) {
+              quantitySold.push(productName);
+              productsMap[productName] = 0;   
+          }
 
-    }
+          productsMap[productName] = productsMap[productName] + quantity;
 
-    
-          itemsMap[soldItems] = itemsMap[soldItems] + parseInt(quantity,10);
-
-});
-          console.log(productNames);
-          console.log(itemsMap);
-          return itemsMap;
-      };
+      });
+      
+      //console.log(productMap);
+      console.log(productsMap);
+      return productsMap;
+  };
 
 
  module.exports = function(fileName){
 
    this.productNames = function(callback){
-    var files = findProducts(fileName)
-     callback(null, files);
-
+        var files = findProducts(fileName)
+        callback(null, files);
     };
 
     this.productsSold = function(callback){
-    var files = findProductsSold(fileName)
-     callback(null, files);
+        var files = findProductsSold(fileName)
+        callback(null, files);
     };
+
 };
 
